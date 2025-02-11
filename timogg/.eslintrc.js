56f5673867e79d26720c39858406c11b6837e002
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-02-11 16:59:42 +0900

    chore : ESLint & Prettier

--- timogg/.eslintrc.js
+++ timogg/.eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      'react/prop-types': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-  }
-  
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+};
