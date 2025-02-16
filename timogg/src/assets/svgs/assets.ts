4a6f94703c330d35631a85a780d63faf81a1bb89
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-02-16 19:22:13 +0900

    asset: 에셋 경로 수정, 매칭 페이지 아이콘 추가

---
+++ timogg/src/assets/svgs/assets.ts
@@ -0,0 +1,33 @@
+import { ReactComponent as MikeIcon } from './mike.svg';
+import { ReactComponent as MikeMuteIcon } from './no_mike.svg';
+import { ReactComponent as HeadsetIcon } from './headset.svg';
+import { ReactComponent as TopPositionIcon } from './top_position.svg';
+import { ReactComponent as JunglePositionIcon } from './jungle_position.svg';
+import { ReactComponent as MidPositionIcon } from './mid_position.svg';
+import { ReactComponent as SupportPositionIcon } from './support_position.svg';
+import { ReactComponent as BottomPositionIcon } from './bottom_position.svg';
+import { ReactComponent as AnyPositionIcon } from './any_position.svg';
+import { ReactComponent as DiscordLogo } from './discord-logo.svg';
+import { ReactComponent as GoogleLogo } from './google-logo.svg';
+import { ReactComponent as KakaoLogo } from './kakao-logo.svg';
+import { ReactComponent as NaverLogo } from './naver-logo.svg';
+import { ReactComponent as TimoLogo } from './logo.svg';
+import { ReactComponent as Star } from './star.svg';
+
+export {
+  MikeIcon,
+  MikeMuteIcon,
+  HeadsetIcon,
+  TopPositionIcon,
+  JunglePositionIcon,
+  MidPositionIcon,
+  SupportPositionIcon,
+  BottomPositionIcon,
+  AnyPositionIcon,
+  DiscordLogo,
+  GoogleLogo,
+  KakaoLogo,
+  NaverLogo,
+  TimoLogo,
+  Star,
+};
