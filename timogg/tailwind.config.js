6a9a0f20e6365f46476c0681e5f07f1163fa8d2f
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-02-23 01:37:23 +0900

    chore: pxtorem 추가
    테일윈드의 단위를 자동으로 px에서 rem으로 바꿔주는 라이브러리

--- timogg/tailwind.config.js
+++ timogg/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import pxtoRem from 'tailwindcss-preset-px-to-rem';
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [pxtoRem],
   theme: {
     extend: {
       fontFamily: {
